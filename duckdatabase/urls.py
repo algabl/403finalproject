520083fd6b1f43d631142324c901652d95efaf12
algabl <103858110+algabl@users.noreply.github.com>
2022-10-26 18:47:27 -0600

    changed things according to what the TA said

--- duckdatabase/urls.py
+++ duckdatabase/urls.py
@@ -1,10 +1,11 @@
 from operator import index
 from django.urls import path
-from .views import indexPageView, aboutPageView, contactPageView, searchPageView
+from .views import indexPageView, aboutPageView, addPageView, searchPageView, confirmationPageView
 
 urlpatterns = [
     path("", indexPageView, name = "index"),
     path("about", aboutPageView, name = "about"),
-    path("contact", contactPageView, name = "contact"),
+    path("add", addPageView, name = "add"),
     path("search", searchPageView, name = "search"),
+    path("confirmation", confirmationPageView, name = "confirmation"),
 ]
--- duckdatabase/views.py
+++ duckdatabase/views.py
@@ -1,13 +1,17 @@
 from django.http import HttpResponse
 
 def indexPageView(request) :
+    # display, update, create, delete
     return HttpResponse('Index')
 
-def contactPageView(request) :
-    return HttpResponse('Contact')
+def addPageView(request) :
+    return HttpResponse('Add')
 
 def aboutPageView(request) :
     return HttpResponse('About')
 
 def searchPageView(request) :
     return HttpResponse('Search')
+
+def confirmationPageView(request) :
+    return HttpResponse('Confirmation')
