36b89da9f763ec41f659b63102f5d9823b4eac7f
algabl <103858110+algabl@users.noreply.github.com>
2022-11-14 20:10:55 -0700

    hopefully these changes don't break anything

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR,'duckdatabase/static')
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'finalproject/static')
 ]
 
 # Default primary key field type
